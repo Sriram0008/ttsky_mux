f10b1e3c8bbadebf5523f403984ae11ece876153
SRIRAM M R <mrsriram195@gmail.com>
2026-04-27 09:50:25 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,35 +1,24 @@
-`timescale 1ns/1ps
-module tb_mux4to1;
+module tb;
+  reg  [7:0] ui_in;
+  wire [7:0] uo_out;
+  reg  [7:0] uio_in;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+  reg ena;
+  reg clk;
+  reg rst_n;
 
-reg  [3:0] d;
-reg  [1:0] s;
-wire y;
+  tt_um_mux4to1 dut (
+    .ui_in(ui_in),
+    .uo_out(uo_out),
+    .uio_in(uio_in),
+    .uio_out(uio_out),
+    .uio_oe(uio_oe),
+    .ena(ena),
+    .clk(clk),
+    .rst_n(rst_n)
+  );
 
-mux4to1 dut (
-    .d(d),
-    .s(s),
-    .y(y)
-);
-
-initial begin
-    d = 4'b1010;
-
-    s = 2'b00; #10;
-    s = 2'b01; #10;
-    s = 2'b10; #10;
-    s = 2'b11; #10;
-
-    d = 4'b1101;
-    s = 2'b00; #10;
-    s = 2'b01; #10;
-    s = 2'b10; #10;
-    s = 2'b11; #10;
-
-    $finish;
-end
-
-initial begin
-    $monitor("time=%0t d=%b s=%b y=%b", $time, d, s, y);
-end
-
+  initial clk = 0;
+  always #5 clk = ~clk;
 endmodule
