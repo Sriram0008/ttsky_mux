df03c39e2a1c9069be4f0b25b4af08ec84b6420e
SRIRAM M R <mrsriram195@gmail.com>
2026-04-27 09:50:02 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,8 +1,20 @@
-module mux4to1(
-    input  wire [3:0] d,
-    input  wire [1:0] s,
-    output reg  y
+module tt_um_mux4to1 (
+    input  [7:0] ui_in,
+    output [7:0] uo_out,
+    input  [7:0] uio_in,
+    output [7:0] uio_out,
+    output [7:0] uio_oe,
+    input        ena,
+    input        clk,
+    input        rst_n
 );
+
+wire [3:0] d;
+wire [1:0] s;
+reg y;
+
+assign d = ui_in[3:0];
+assign s = ui_in[5:4];
 
 always @(*) begin
     case (s)
@@ -13,5 +25,11 @@ always @(*) begin
         default: y = 1'b0;
     endcase
 end
+
+assign uo_out = {7'b0000000, y};
+assign uio_out = 8'b00000000;
+assign uio_oe  = 8'b00000000;
+
+wire _unused = &{ena, clk, rst_n, uio_in};
 
 endmodule
